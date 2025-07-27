51bec11b0cfabbfa17117691cc1846f7c4590396
Reid W <reiddwilliamson@gmail.com>
2025-07-27 16:39:22 -0700

    added clock

--- main.c
+++ main.c
@@ -1,24 +1,42 @@
 #include <arm_neon.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdint.h>
 
-#define ROWS 6
-#define COLS 6
 
-int matrix[ROWS][COLS] = {
-    {1,2,3,4,5,6},
-    {1,2,3,4,5,6},
-    {1,2,3,4,5,6},
-    {1,2,3,4,5,6},
-    {1,2,3,4,5,6},
-    {1,2,3,4,5,6},
+#define ROWS 8
+#define COLS 8
+
+// matrix holds 11-bit integers
+int16_t matrix[ROWS][COLS] = {
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+    {1,2,3,4,5,6,7,8},
+};
+
+// identity matrix that will become our answer
+int16_t identity[ROWS][COLS] = {
+    {1,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0},
+    {0,0,1,0,0,0,0,0},
+    {0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,0,1,0,0},
+    {0,0,0,0,0,0,1,0},
+    {0,0,0,0,0,0,0,1},
 };
 
 /*
- *Optimizations: Loop unrolling
+ * Possible Optimizations: Loop unrolling, neon intrinsics
  *
 */
-float matrix_norm(){
+int matrix_norm(){
     int max = 0;
     for(int row = 0; row < ROWS; row++){
         int sum = 0;
@@ -34,6 +52,28 @@ float matrix_norm(){
     return max;
 }
 
+/*
+ For each pivot row
+    Find the pivot element A[i][i]
+    If the pivot is zero, swap with another row
+    Normalize pivot row by dividing to make pivot = 1
+    For every other row in the column, eliminate A[j][i] by subtracting A[j][i] x pivot row from row j.
+*/
+int inverter(){
+    for(int row = 0; row < ROWS; row++){
+
+    }
+}
+
 int main(){
-    printf("norm: %f", matrix_norm());
+    clock_t start = clock();
+
+    inverter();
+
+    clock_t end = clock();
+
+    double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Elapsed time: %f seconds\n", time_spent);
+
+    exit(0);
 }
